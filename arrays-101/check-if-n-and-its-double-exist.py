8726da47a4eccbe2c6599e701460de5deefa9e2b
uygarrr <45368613+uygarrr@users.noreply.github.com>
2022-02-02 17:03:01 +0300

    Add files via upload

---
+++ arrays-101/check-if-n-and-its-double-exist.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr) - 1):
+            for j in range(i+1, len(arr)):
+                if arr[i] == arr[j] * 2 or arr[i] * 2 == arr[j]:
+                    return True
+                
+        return False

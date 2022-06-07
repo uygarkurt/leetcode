5ab6b7a2db504fba4db596021767c773906bf2e0
uygarrr <45368613+uygarrr@users.noreply.github.com>
2022-06-07 10:23:02 +0300

    Delete binary-search.py directory

--- binary-search.py/median-of-two-sorted-arrays.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1.extend(nums2)
-        nums1 = sorted(nums1)
-        mid = (len(nums1) -1) // 2
-        if len(nums1) % 2 == 1:
-            return nums1[mid]
-        else:
-            return (nums1[mid] + nums1[mid+1]) / 2
--- binary-search.py/sqrtx.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-        for i in range(x+1):
-            if i * i <= x and (i+1) * (i+1) > x:
-                return i
